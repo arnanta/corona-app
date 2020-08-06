816d557ada08105805b82fd70ae422e79dea5d14
arnanta <arnantachatterjee@gmail.com>
2020-08-06 13:56:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Cards from './Components/Cards/Cards';
+import Chart from './Components/Chart/Chart';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {fetchData} from './api';
+// Try for one import statement
+
+
+
+class App extends React.Component{
+state={
+    data:{},
+    country:'',
 }
 
+    async componentDidMount(){
+
+        const fetchedData=await fetchData();
+        //console.log(data);
+        this.setState({data:fetchedData});
+
+
+    }
+
+handleCountryChange= async(country)=>{
+    // fetch the data
+    //set the data
+    const fetchedData=await fetchData(country);
+    this.setState({data:fetchedData,country:country});
+
+
+   // console.log(country);
+}
+
+    render(){
+        // eslint-disable-next-line
+
+        const {data,country}=this.state;
+        return (
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country} />
+                
+            </div>
+        )
+    }
+}
 export default App;
